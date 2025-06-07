60309ad3dad142f71a71c9d18e7d8b4e7a77c859
baozengkai598 <baozengkai598@hellobike.com>
2025-06-07 10:02:03 +0800

    first code

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 )
 
 func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	// 1.变量
+	testVar := "gopher"
+	fmt.Printf("Hello and welcome, %s!\n", testVar)
 
 	for i := 1; i <= 5; i++ {
 		fmt.Println("i =", 100/i)
 	}
+
+	// 2.常量
+	const pi = 3.1415926
+	fmt.Println(pi)
 }
